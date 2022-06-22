fa7dcac59d2f772f9f4df6e7e6862eca27e363fd
SupreethaD <supreethad@gmail.com>
2022-06-22 17:14:43 -0500

    commitredis

--- app.py
+++ app.py
@@ -72,66 +72,51 @@ def sequencerangequery2():
  return render_template('sequenceRange2.html', rangeoutputval=rangeresult, timeelapsed = time_elapsed)
 
 #query 12
-@app.route('/query7witoutcache')
+@app.route('/query7')
 def query7_withoutcache():
-    inputnumber = str(request.args.get('numberfield1'))
+    number1 = str(request.args.get('number1'))
 
-    starttime = time()
-    for z in range(int(inputnumber)):
+    start = time()
+    for z in range(int(number1)):
         cursor.execute("select volcano_name, country, region, latitude, longitude, elev from volcano where number in (select number from volcanoindex where sequence >= 1000 and Sequence <= 1500);")
-        rangevalues = cursor.fetchall()
+        values = cursor.fetchall()
         cursor.execute("select top 5 volcano_Name, country, region, latitude, longitude, elev from volcano order by number desc;")
-        rangeresult = cursor.fetchall()
+        result = cursor.fetchall()
 
-    endtime = time()
-    totaltime = endtime - starttime
-    return render_template('query7witoutcache.html', timetaken = totaltime) 
+    end = time()
+    total = end - start
+    return render_template('query7.html', timetaken = total) 
 
 #query 13
-@app.route('/query8withcache')
+@app.route('/query8')
 def query2withcache():
-    randcachenum = str(request.args.get('numberfield1'))
-    startrange = '100'
-    endrange = '100000'
-    startelevation = '100'
-    endelevation = '100000'
-    sql_qr1 = "SELECT volcano_name, country, region, latitude, longitude, elev FROM v WHERE number >= "+startrange+" and number <= "+endrange+" and elev >= "+startelevation+" and elev <= "+endelevation+";"
-    sql_qr2 = "SELECT avg(elev) FROM v WHERE number >= "+startrange+" and number <= "+endrange+" and elev >= "+startelevation+" and elev <= "+endelevation+" group by elev;"
-    sql_qr3 = "select volcano_name, country, region, latitude, longitude, elev from v where number in (select number from vindex where sequence >= 1000 and Sequence <= 1500);"
-    sql_qr4 = "select top 5 volcano_Name, country, region, latitude, longitude, elev from v order by number desc;"
+    randcachenum = str(request.args.get('number2'))
+    sql_qr1 = "select volcano_name, country, region, latitude, longitude, elev from volcano where number in (select number from volcanoindex where sequence >= 1000 and Sequence <= 1500);"
+    sql_qr2 = "select top 5 volcano_Name, country, region, latitude, longitude, elev from volcano order by number desc;"
     
-    hash_val1 = hashlib.sha224(sql_qr1.encode('utf-8')).hexdigest()
-    hash_val2 = hashlib.sha224(sql_qr2.encode('utf-8')).hexdigest()
-    hash_val3 = hashlib.sha224(sql_qr3.encode('utf-8')).hexdigest()
-    hash_val4 = hashlib.sha224(sql_qr4.encode('utf-8')).hexdigest()
-    redis_ky1 = 'redis_cache1:' + hash_val1
-    redis_ky2 = 'redis_cache2:' + hash_val2
-    redis_ky3 = 'redis_cache3:' + hash_val3
-    redis_ky4 = 'redis_cache4:' + hash_val4
+    hash1 = hashlib.sha224(sql_qr1.encode('utf-8')).hexdigest()
+    hash2 = hashlib.sha224(sql_qr2.encode('utf-8')).hexdigest()
+
+    redis1 = 'redis_cache1:' + hash1
+    redis2 = 'redis_cache2:' + hash2
     startcount = time()
     
     for qry in range(int(randcachenum)):
-        if(r.get(redis_ky1) and r.get(redis_ky2) and r.get(redis_ky3) and r.get(redis_ky4)):
+        if(r.get(redis1) and r.get(redis2)):
             print("CACHING REDIS")
         else:
             print("Inside else")
             cursor.execute(sql_qr1)
             cursor.execute(sql_qr2)
-            cursor.execute(sql_qr3)
-            cursor.execute(sql_qr4)
             outputcachedres = cursor.fetchall()
-            r.set(redis_ky1, pickle.dumps(outputcachedres))
-            r.set(redis_ky2, pickle.dumps(outputcachedres))
-            r.set(redis_ky3, pickle.dumps(outputcachedres))
-            r.set(redis_ky4, pickle.dumps(outputcachedres))
-            r.expire(redis_ky1, 60)
-            r.expire(redis_ky2, 60)
-            r.expire(redis_ky3, 60)
-            r.expire(redis_ky4, 60)
+            r.set(redis1, pickle.dumps(outputcachedres))
+            r.set(redis2, pickle.dumps(outputcachedres))
+            r.expire(redis1, 60)
+            r.expire(redis2, 60)
         
     endcount = time()
     totcount = endcount - startcount
-    return render_template("query8withcache.html", enhancedrendertime = totcount)
+    return render_template("query8.html", enhancedrendertime = totcount)
 
 if __name__ == "__main__":
     app.run()
